0d1ef9850750a9b0b3e3baf41a66cd612a459be9
holickka <64195032+holickka@users.noreply.github.com>
2023-10-20 15:45:16 +0800

    Add files via upload
    added ft_strncmp

---
+++ ft_strncmp.c
@@ -0,0 +1,25 @@
+// #include "libft.h"
+#include <stdlib.h>
+
+int ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while ((s1[i] || s2[i]) && i < n )
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
+}
+
+#include <stdio.h>
+#include <string.h>
+
+int	main()
+{
+	printf("%d\n", ft_strncmp("banana", "baza", 0));
+}
+
