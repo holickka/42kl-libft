15a9e5a198441955db49838ccf861d2677630b0a
holickka <64195032+holickka@users.noreply.github.com>
2023-11-06 19:05:51 +0800

    Add files via upload
    add max number constrain to function

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,13 +6,14 @@
 /*   By: hsim <hsim@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 19:59:26 by hsim              #+#    #+#             */
-/*   Updated: 2023/10/27 19:59:27 by hsim             ###   ########.fr       */
+/*   Updated: 2023/11/06 19:04:57 by hsim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 #include <unistd.h>
+#define UINT_MAX 4294967295
 
 void	*ft_calloc(size_t count, size_t n)
 {
@@ -20,6 +21,8 @@ void	*ft_calloc(size_t count, size_t n)
 	unsigned char	*tab;
 
 	i = 0;
+	if (n != 0 && count > UINT_MAX / n)
+		return (NULL);
 	tab = (unsigned char *)malloc(count * n);
 	if (tab != NULL)
 	{
@@ -34,17 +37,21 @@ void	*ft_calloc(size_t count, size_t n)
 int	main()
 {
 	int	*arr;
-	int	i;
-	int	n;
+	unsigned int	i;
+	unsigned int	n;
 
-	n = -2147483647;
+	n = 4294967294;
 	i = 0;
-	arr = (int *)ft_calloc(n, sizeof(int));
-	while(i < n)
-		printf("%d ", arr[i++]);
+	arr = (int *)calloc(69999, 69999);
+
+//	while(i < n)
+//		printf("%d ", arr[i++]);
 }
 */
 /*
  allocate space for block count in byte size as defined in n
  allocated mem is filled with bytes val of 0
- */
+ system malloc max size = sq of 2147483647
+ system calloc max size = sq of 69999 ~ 700000
+ mine function define max size = sq of 65535 (size_t max)
+*/
