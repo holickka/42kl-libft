8600b4d896aefbf4da26629b8a3810a047adf953
holickka <64195032+holickka@users.noreply.github.com>
2025-01-04 18:08:03 +0800

    modified ft_perror_fd to remove the '\n'. we now need to add \n together when drafting the message

--- ft_perror_fd.c
+++ ft_perror_fd.c
@@ -6,7 +6,7 @@
 /*   By: hsim <hsim@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 07:51:12 by hsim              #+#    #+#             */
-/*   Updated: 2025/01/03 07:57:43 by hsim             ###   ########.fr       */
+/*   Updated: 2025/01/04 17:38:17 by hsim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,5 @@ int	ft_perror_fd(char *s, int fd, int value)
 	if (!s)
 		return (0);
 	write(fd, s, ft_strlen(s));
-	write(fd, "\n", 1);
 	return (value);
 }
